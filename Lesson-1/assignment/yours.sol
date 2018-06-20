c0d8bdbbf97241cf5d6fd8b09fffa5ebd1179607
Damon <damononliu@users.noreply.github.com>
2018-06-20 18:31:52 +0800

    Update yours.sol

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -2,51 +2,54 @@
 pragma solidity ^0.4.14;
 
 contract Payroll {
-    uint constant payDuration = 10 seconds;
 
-    address owner;
     uint salary;
-    address employee;
+    uint constant payDuration = 10 seconds;
     uint lastPayday;
-
+    address owner;
+    address employee;
+    
     function Payroll() {
-        owner = msg.sender;
-    }
-
-    function updateEmployee(address e, uint s) {
-        require(msg.sender == owner);
-
-        if (employee != 0x0) {
-            uint payment = salary * (now - lastPayday) / payDuration;
-            employee.transfer(payment);
-        }
-
-        employee = e;
-        salary = s * 1 ether;
-        lastPayday = now;
+        owner == msg.sender;
     }
 
     function addFund() payable returns (uint) {
         return this.balance;
     }
-
+    
     function calculateRunway() returns (uint) {
         return this.balance / salary;
     }
-
+    
     function hasEnoughFund() returns (bool) {
         return calculateRunway() > 0;
     }
-
+    
     function getPaid() {
-        require(msg.sender == employee);
-
-        uint nextPayday = lastPayday + payDuration;
-        assert(nextPayday < now);
-
-        lastPayday = nextPayday;
-        employee.transfer(salary);
+        if(msg.sender != employee) {
+            revert();
+        }
+        uint nextPayDay = lastPayday + payDuration;
+        if(nextPayDay < now) {
+            lastPayday = nextPayDay;
+            employee.transfer(salary);
+        } else {
+            revert();
+        }
     }
     
-    
+    function updateAddress(address a, uint s) {
+        if(msg.sender != owner) {
+            revert();
+        }
+        if(employee != 0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+            lastPayday = now;
+        }
+        
+        employee = a;
+        salary = s * 1 ether;
+
+    }
 }
