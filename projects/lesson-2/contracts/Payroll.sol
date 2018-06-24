373bbabf58879032ff47c10b4dc98a9edf81f121
Liu <>
2018-06-24 11:40:25 +0800

    lesson2 作业提交

--- projects/lesson-2/contracts/Payroll.sol
+++ projects/lesson-2/contracts/Payroll.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.4.14;
 contract Payroll {
 
     struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
         // TODO: your code here
     }
 
@@ -15,27 +18,63 @@ contract Payroll {
         owner = msg.sender;
     }
 
-    function addEmployee(address employeeAddress, uint salary) public {
+    function _partialPaid(Employee employee) private{
+        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
+    }
+
+    function _findEmployee(address employeeId) private returns (Employee, uint){
+        for(uint i=0; i < employees.length; i++){
+            if(employees[i].id == employeeId){
+                return (employees[i], i);
+            }
+        }
+    }
+
+    function addEmployee(address employeeId, uint salary) public {
         require(msg.sender == owner);
         // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+
+        assert(employee.id == 0x0);
+
+        employees.push(Employee(employeeId, salary *1 ether, now));
     }
 
     function removeEmployee(address employeeId) public {
         require(msg.sender == owner);
         // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id != 0);
+
+        _partialPaid(employee);
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
     }
 
-    function updateEmployee(address employeeAddress, uint salary) public {
+    function updateEmployee(address employeeId, uint salary) public {
         require(msg.sender == owner);
         // TODO: your code here
+        var (employee, index) = _findEmployee(employeeId);
+        assert(employee.id == 0x0);
+
+        _partialPaid(employee);
+        employees[index].salary = salary * 1 ether;
+        employees[index].lastPayday = now;
     }
 
     function addFund() payable public returns (uint) {
-        return address(this).balance;
+        return this.balance;
     }
 
     function calculateRunway() public view returns (uint) {
         // TODO: your code here
+        uint totalSalary = 0;
+        for (uint i = 0; i < employees.length; i++){
+            totalSalary += employees[i].salary;
+        }
+        return this.balance / totalSalary ;
     }
 
     function hasEnoughFund() public view returns (bool) {
@@ -44,6 +83,15 @@ contract Payroll {
 
     function getPaid() public {
         // TODO: your code here
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0);
+
+        uint nextPayday = employee.lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        employees[index].lastPayday = nextPayday;
+        employee.id.transfer(employee.salary);
+
     }
 }
 
---
+++ projects/lesson-2/home-work.md
@@ -0,0 +1,20 @@
+### 作业
+
+* 加入第一个员工 `calculateRunway()` 函数 ，消耗的 gas     
+* transaction cost 22974 gas  execution cost 1702 gas
+* 加入第二个员工 `calculateRunway()` 函数 ，消耗的 gas 
+* transaction cost 23755 gas  execution cost 2483 gas
+* 加入第三个员工 `calculateRunway()` 函数 ，消耗的 gas 
+* transaction cost 25317 gas  execution cost 4045 gas
+* 加入第四个员工 `calculateRunway()` 函数 ，消耗的 gas 
+* transaction cost 26098 gas  execution cost 4826 gas
+* 加入第五个员工 `calculateRunway()` 函数 ，消耗的 gas 
+* transaction cost 26879 gas  execution cost 5607 gas
+* 加入第二个员工 `calculateRunway()` 函数 ，消耗的 gas 
+
+* 随着 员工人数增加 遍历所有人要消耗越来越多的gas
+
+* 把所有的员工和员工的薪水做到mapping里，以减少gas的消耗
+
+
+
